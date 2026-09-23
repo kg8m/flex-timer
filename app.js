bd5c15756740eda4b5a93e790e5e476cb50b7bdc
kg8m <takumi.kagiyama@gmail.com>
2026-09-23 16:40:55 +0900

    Add download/upload icons to header menu’s `Export data...`/`Import data...`
    Plain text rows made the two items easy to confuse at a glance. They now lead with download/upload
    icons, added to `Icons` and attached via `data-icon`, sized down to 16px to suit the menu’s text
    rows.

--- app.js
+++ app.js
@@ -611,6 +611,16 @@
         <line x1="17.7" y1="2.5" x2="17.7" y2="8.1"></line>
         <line x1="14.9" y1="5.3" x2="20.5" y2="5.3"></line>
       `,
+      download: html`
+        <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+        <polyline points="7 10 12 15 17 10"></polyline>
+        <line x1="12" y1="15" x2="12" y2="3"></line>
+      `,
+      upload: html`
+        <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+        <polyline points="17 8 12 3 7 8"></polyline>
+        <line x1="12" y1="3" x2="12" y2="15"></line>
+      `,
     };
 
     /**
